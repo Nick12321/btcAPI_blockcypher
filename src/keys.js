2f09c34fafacfa09629574f749ef5c7df4af2774
Nick12321 <Nick.thompson@rogers.com>
2018-09-02 22:39:33 -0400

    add require libraries

---
+++ src/keys.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var crypto = require('crypto');
+var base58 = require('bs58');
+var ecurve = require('ecurve');
+var BigInteger = require('bigi');
+var getRandomValue = require('get-random-value');
